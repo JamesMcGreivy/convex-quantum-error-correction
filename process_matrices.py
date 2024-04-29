d211702faa22aea495d0c614aad3eefaadf28e25
James McGreivy <jamesmcgreivy@dhcp-10-31-188-105.dyn.MIT.EDU>
2024-04-29 18:40:27 -0400

    first commit

---
+++ process_matrices.py
@@ -0,0 +1,52 @@
+"""
+File: QuantumErrorCorrector.py
+Author: James McGreivy
+Date: 04/29/2024
+Description: A pytorch module which wraps the process matrices X_C, X_E, and X_R. 
+"""
+import numpy as np
+import torch
+import opt_einsum
+import itertools
+
+
+class ProcessMatrices(torch.nn.Module):
+    def __init__(self, q_s, q_c, K = None, X_E = None):
+        super().__init__()
+        self.q_s = q_s
+        self.q_c = q_c
+
+        self.n_s = 2**q_s
+        self.n_c = 2**q_c
+
+        self.V_C = torch.nn.Parameter(torch.normal(mean = 0, std = 1 / (self.n_c**2 * self.n_s**2), size = (self.n_c, self.n_s, self.n_c, self.n_s), dtype = torch.complex128))
+        self.V_R = torch.nn.Parameter(torch.normal(mean = 0, std = 1 / (self.n_c**2 * self.n_s**2), size = (self.n_s, self.n_c, self.n_s, self.n_c), dtype = torch.complex128))
+
+        if (K is None) == (X_E is None):
+            raise ValueError("Please provide either K or X_E")
+        if X_E is None:
+            self.X_E = self.krauss_to_X_E(K)
+        else:
+            self.X_E = X_E 
+        self.X_E.requires_grad = False
+
+    # Returns the process matrices for the coding, error, and recovery channel
+    def forward(self):
+        X_C = opt_einsum.contract("pqmi,pqsj->misj", self.V_C.conj(), self.V_C)
+        X_E = self.X_E
+        X_R = opt_einsum.contract("pqil,pqjg->iljg", self.V_R.conj(), self.V_R)
+        return X_C, X_E, X_R 
+
+    # Constructs X_E, the error process matrix, given a set of 2x2 Krauss operators describing a single-qubit error channel.
+    def krauss_to_X_E(self, K):
+        E = torch.zeros(size=self.q_c*[4] + 2*[self.n_c], dtype = torch.complex128)
+
+        for i in itertools.product(*[range(0,len(K)) for _ in range(0, self.q_c)]):
+            K_tot = K[i[0]]
+            for j in range(1, self.q_c):
+                K_tot = torch.kron(K_tot, K[i[j]])
+            E[i] = K_tot
+
+        X_E = opt_einsum.contract("".join([str(i) for i in range(self.q_c)]) + "lm," + "".join([str(i) for i in range(self.q_c)]) + "gs->lmgs", E, E.conj())
+        return X_E
+
---
+++ utils.py
@@ -0,0 +1,21 @@
+"""
+File: utils.py
+Author: James McGreivy
+Date: 04/29/2024
+Description: Some utility functions for the optimization.
+"""
+import numpy as np
+import torch
+import opt_einsum
+import itertools
+
+# Average fidelity loss function
+def avg_fidelity(X_C, X_E, X_R):
+    return opt_einsum.contract("iljg,misj,lmgs->", X_R, X_C, X_E).real
+
+# Regularization term equal to zero if X, the process matrix, sums up to the identity as required of a quantum channel.
+def sums_to_identity(X):
+    d = torch.eye(X.shape[-1])
+    normalization = (opt_einsum.contract("ijik->jk",X) - d)
+    normalization = (normalization.conj() * normalization).real
+    return opt_einsum.contract("jk->", normalization)
